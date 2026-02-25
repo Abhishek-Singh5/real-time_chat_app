982f97ded8053884f27715212e5243ff3c2c0392
Abhishek Singh <as942501@gmail.com>
2026-02-26 01:33:39 +0530

    axios updatess

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
 
-    baseURL: import.meta.env.MODE === "production" ? "https://real-time-chat-app-ssvq.onrender.com/api" : "/api",
+    baseURL: import.meta.env.MODE === "production" ? "https://real-time-chat-8hpvgsqlv-abhishek-singh5s-projects.vercel.app/api" : "/api",
     withCredentials: true,
 
 });
